a52dcb04350b9c52fbc70f3c468a0400cfd76935
Deepthi <11665833+deeptijnair@users.noreply.github.com>
2025-07-17 13:49:21 -0400

    Improved the Reset Button functionality

--- bmi.js
+++ bmi.js
@@ -7,25 +7,23 @@
 // }
 
 
-
 function calculateBMI(){
     let height = document.getElementById("height").value;
     let weight = document.getElementById("weight").value;
     let result = document.getElementById("result");
+
+
     
-    if(height && weight) {
+    if (height <= 0 || weight <= 0) {
+    result.innerHTML = "Please enter valid height and weight.";
+    // return;
+
+    }else if(height && weight) {
         let heightInMeters = height / 100; // Convert height from cm to meters
         let bmi = (weight) / (heightInMeters * heightInMeters);
         document.getElementById("bmiValue").innerHTML = bmi.toFixed(2);
-        document.getElementById("calculateBmi").disabled = false;
-        document.getElementById("resetButton").disabled = false;
-
-}
-
-if (height <= 0 || weight <= 0) {
-    result.innerHTML = "Please enter valid height and weight.";
-    return;
-}
+ 
+    }
 
 }
 
@@ -33,6 +31,6 @@ function resetForm(){
     document.getElementById("height").value = "";
     document.getElementById("weight").value = "";
     document.getElementById("bmiValue").innerHTML = "";
-    document.getElementById("result").innerHTML = "Result:";
+    result.innerHTML = "Result:"
 
 }
